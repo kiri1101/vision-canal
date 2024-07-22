36963cbb66dffd1c82e2eedc1adece2b184811de
Kiri1101 <j.folem@adwa.cm>
2024-07-22 10:54:32 +0100

    feature: implementing order list table

---
+++ app/Http/Requests/OrderListRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Carbon\Carbon;
+use App\Http\Traits\Helpers;
+use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderListRequest extends FormRequest
+{
+    use Helpers;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'start' => strlen($this->input('start')) > 0 ? 'date' : '',
+            'end' => strlen($this->input('end')) > 0 ? 'date' : ''
+        ];
+    }
+
+    public function search(): JsonResponse
+    {
+        $startDate = strlen($this->input('start')) > 0 ? Carbon::createFromFormat('Y-m-d', $this->input('start')) : '';
+        $endDate = strlen($this->input('end')) > 0 ? Carbon::createFromFormat('Y-m-d', $this->input('end')) : '';
+
+        $query = Order::query();
+
+        if (strlen($startDate) === 0 && strlen($endDate) === 0) {
+            $query->latest('created_at')->take(100)->get();
+        } else if (strlen($startDate) === 0 && strlen($endDate) > 0) {
+            $query->whereDate('created_at', '<=', $endDate)->latest('created_at')->take(100)->get();
+        } else if (strlen($startDate) > 0 && strlen($endDate) === 0) {
+            $query->whereDate('created_at', '>=', $startDate)->latest('created_at')->take(100)->get();
+        } else {
+            $query->whereDate('created_at', '>=', $startDate)->whereDate('created_at', '<=', $endDate)->latest('created_at')->take(100)->get();
+        }
+
+        // $orderList = $query->where('status', false)->get();
+        $orderList = $query->get();
+
+        return $this->successResponse('Orders list', [
+            'number' => $orderList->count(),
+            'list' => $orderList->map(fn ($order) => [
+                'id' => $order->uuid,
+                'user' => $order->user->name,
+                'tel' => $order->user->phone,
+                'article' => $order->article->title,
+                'status' => $order->status,
+                'created' => $order->created_at->format('d/m/Y \a H:i')
+            ])->all()
+        ]);
+    }
+}
---
+++ resources/js/Pages/Dashboard/OrderList.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="px-0 tracking-tight md:px-4 lg:px-6">
+        <div class="py-4 bg-white border-b border-gray-200 mdx1:flex sm:justify-between lg:px-8">
+            <h1 class="text-xl font-semibold text-gray-800">Order's List</h1>
+
+            <form @submit.prevent="filteringSupport" class="flex-wrap items-end gap-3 mt-2 sm:flex">
+                <div class="gap-3 sm:flex">
+                    <div>
+                        <label for="date-start" class="block text-sm"> Start Date </label>
+
+                        <Calendar v-model="query.start" showIcon iconDisplay="input" inputId="startDate"
+                            date-format="dd/mm/yy" :pt="{
+                                root: {
+                                    class: 'w-full rounded h-9',
+                                },
+                                input: {
+                                    class: 'w-full rounded h-9 border border-gray-300 shadow-sm',
+                                },
+                            }" />
+                    </div>
+                    <div>
+                        <label for="end-date" class="block text-sm"> End Date </label>
+
+                        <Calendar v-model="query.end" showIcon iconDisplay="input" inputId="endDate"
+                            date-format="dd/mm/yy" :pt="{
+                                root: {
+                                    class: 'w-full rounded h-9',
+                                },
+                                input: {
+                                    class: 'w-full rounded h-9 border border-gray-300 rounded-md shadow-sm',
+                                },
+                            }" />
+                    </div>
+                </div>
+
+                <PrimaryButton class="w-full mt-5 sm:w-auto h-9">
+                    <span>Search</span>
+                    <!-- <i class="block sm:hidden fa-solid fa-magnifying-glass"></i> -->
+                </PrimaryButton>
+            </form>
+        </div>
+
+        <div class="overflow-hidden">
+            <div class="text-xs card">
+                <Toast />
+
+                <div v-if="isLoading" class="min-h-80">
+                    <Skeleton class="my-2 rounded" height="4rem"></Skeleton>
+
+                    <Skeleton class="my-2 rounded" height="16rem"></Skeleton>
+                </div>
+
+                <div v-else class="min-h-80">
+                    <div v-if="products.length === 0" class="grid place-items-center h-96">
+                        <div class="text-center">
+                            <i class="text-2xl fa-regular fa-folder-open"></i>
+                            <p>No orders</p>
+                        </div>
+                    </div>
+
+                    <DataTable v-else ref="dt" :value="products" v-model:selection="selectedUsers" dataKey="id"
+                        :paginator="true" :rows="10" :filters="filters"
+                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                        :rowsPerPageOptions="[5, 10, 25]"
+                        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} Transactions"
+                        class="font-futura">
+                        <template #header>
+                            <div
+                                class="flex flex-wrap items-center justify-between gap-2 align-items-center justify-content-between">
+                                <!-- <h4 class="m-0 text-sm capitalize sm:text-base">
+                                Transactions
+                            </h4> -->
+                                <IconField iconPosition="left">
+                                    <InputIcon>
+                                        <i class="pi pi-search" />
+                                    </InputIcon>
+                                    <InputText v-model="filters['global'].value"
+                                        class="w-full border border-gray-300 rounded shadow-sm h-9 indent-5"
+                                        placeholder="Search..." />
+                                </IconField>
+                            </div>
+                        </template>
+
+                        <Column selectionMode="multiple" class="w-12" :exportable="false"> </Column>
+                        <Column field="user" header="Name of user" sortable class="min-w-[10rem] whitespace-nowrap">
+                        </Column>
+                        <Column field="tel" header="Phone Number" sortable class="min-w-[10rem] whitespace-nowrap">
+                        </Column>
+                        <Column field="article" header="Article" sortable class="min-w-[10rem] whitespace-nowrap">
+                        </Column>
+                        <Column header="Status">
+                            <template #body="slotProps">
+                                <Tag v-if="slotProps.data.status" value="Handled" severity="success" />
+                                <Tag v-else value="Pending" severity="warning" class="cursor-pointer"
+                                    @click.prevent="toggleOverlayPanel" />
+
+                                <OverlayPanel ref="op" class="bg-white cursor-pointer hover:bg-emerald-300">
+                                    <p @click.prevent="(e) => handlingStatusToggle(e, slotProps.data.id)">
+                                        Handled
+                                    </p>
+                                </OverlayPanel>
+                            </template>
+                        </Column>
+                        <Column field="created" header="Created At" sortable class="min-w-[10rem] whitespace-nowrap">
+                        </Column>
+                    </DataTable>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useForm } from "@inertiajs/vue3";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Calendar from "primevue/calendar";
+import { FilterMatchMode } from "primevue/api";
+import Skeleton from "primevue/skeleton";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import InputText from "primevue/inputtext";
+import DataTable from "primevue/datatable";
+import Tag from "primevue/tag";
+import Column from "primevue/column";
+import Toast from "primevue/toast";
+import { useToast } from "primevue/usetoast";
+import OverlayPanel from "primevue/overlaypanel";
+import moment from "moment";
+import { axiosInstance } from "@/mixins/axiosInstance";
+
+onMounted(() => {
+    filteringSupport();
+});
+
+const toast = useToast();
+
+const isLoading = ref(false);
+
+const query = ref({
+    start: "",
+    end: "",
+});
+
+const dt = ref();
+
+const op = ref();
+
+const products = ref([]);
+
+const selectedUsers = ref();
+
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+
+const handlingStatusToggle = (e, id) => {
+    toggleOverlayPanel(e);
+    toggleOrderStatus(id);
+    // settings/orders/{order}/update
+    useForm({}).post(route('settings.order.update', id), {
+        onSuccess: () => { console.log('order status updated') },
+        onError: () => { }
+    })
+}
+
+const toggleOverlayPanel = (event) => op.value.toggle(event);
+
+const formatQueryDate = (value) =>
+    value.toString().length > 0 ? moment(value).format("YYYY-MM-DD") : "";
+
+const toggleOrderStatus = (id) => {
+    console.log('chosen status id: ', id);
+}
+
+const filteringSupport = () => {
+    let startDate = query.value.start === null ? "" : query.value.start;
+    let endDate = query.value.end === null ? "" : query.value.end;
+
+    isLoading.value = true;
+    axiosInstance
+        .post(route("settings.order.list"), {
+            start: formatQueryDate(startDate),
+            end: formatQueryDate(endDate),
+        })
+        .then((response) => {
+            isLoading.value = false;
+            products.value = response.data.data.list;
+        })
+        .catch((error) => {
+            isLoading.value = false;
+            toast.add({
+                severity: "error",
+                summary: "Error",
+                detail: "Cannot load messages",
+                life: 3000,
+            });
+        });
+};
+</script>
