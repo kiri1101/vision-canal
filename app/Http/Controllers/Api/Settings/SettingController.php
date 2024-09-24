538ca6acd028e93ffe164577ed201ad92a8a0438
Kiri1101 <j.folem@adwa.cm>
2024-09-24 13:09:41 +0100

    fix: add page banners to settings table

--- app/Http/Controllers/Api/Settings/SettingController.php
+++ app/Http/Controllers/Api/Settings/SettingController.php
@@ -27,16 +27,14 @@ class SettingController extends BaseController
 
     public function getSettings(): JsonResponse
     {
-        $setting = Setting::all();
-
         return $this->successResponse(Lang::get('messages.success.default_response', [], 'en'), [
-            'services' => Service::all()->map(fn ($service) => [
+            'services' => Service::all()->map(fn($service) => [
                 'service_name' => $service->name,
                 'profile_pic_path' => $service->banner_path,
                 'service_route' => $service->route_name,
                 'service_alt' => $service->alt
             ]),
-            'countries' => Country::all()->map(fn ($country) => [
+            'countries' => Country::all()->map(fn($country) => [
                 'id' => $country->slug,
                 'label' => $country->name,
                 'image_url' => env('APP_URL') . "/public/" . $country->img_path
@@ -46,27 +44,31 @@ class SettingController extends BaseController
             //     'name' => $option->name,
             //     'value' => $option->fee
             // ]),
-            'offers' => Category::isActive()->isNotSpecial()->get()->map(fn ($offer) => [
+            'offers' => Category::isActive()->isNotSpecial()->get()->map(fn($offer) => [
                 'id' => Str::uuid(),
                 'name' => "{$offer->name} ({$offer->fee})",
                 'label' => $offer->name,
                 'value' => $offer->uuid,
                 'fee' => $offer->fee
             ]),
-            'payment_methods' => PaymentMethod::all()->map(fn ($payment) => [
+            'payment_methods' => PaymentMethod::all()->map(fn($payment) => [
                 'id' => Str::uuid(),
                 'label' => $payment->name,
                 'value' => $payment->uuid,
                 'method_banner_path' => env('APP_URL') . "/public/" . $payment->img_path,
                 'method_banner_alt' => $payment->img_alt
             ]),
-            'settings' => [
-                'home_video_link' => $setting->value('home_page_video'),
-                'whatsappLink' => is_null($setting->value('whatsapp')) ? '' : $setting->value('whatsapp'),
-                'facebookLink' => is_null($setting->value('facebook')) ? '' : $setting->value('facebook'),
-                'instagramLink' => is_null($setting->value('instagram')) ? '' : $setting->value('instagram')
-            ],
-            'articles' => Article::all()->map(fn ($article) => [
+            'settings' => Setting::all()->map(fn(object $setting) => [
+                'home_video_link' => $setting->home_page_video,
+                'subscription' => is_null($setting->subscribe_banner) ? '' : $setting->subscribe_banner,
+                'resubscription' => is_null($setting->resubscribe_banner) ? '' : $setting->resubscribe_banner,
+                'accessory' => is_null($setting->accessory_banner) ? '' : $setting->accessory_banner,
+                'technician' => is_null($setting->accessory_banner) ? '' : $setting->technician_banner,
+                'whatsappLink' => is_null($setting->whatsapp) ? '' : $setting->whatsapp,
+                'facebookLink' => is_null($setting->facebook) ? '' : $setting->facebook,
+                'instagramLink' => is_null($setting->instagram) ? '' : $setting->instagram
+            ]),
+            'articles' => Article::all()->map(fn($article) => [
                 'id' => $article->uuid,
                 'article_pic_url' => $article->img_path,
                 'article_title' => $article->title,
